1bfb6aa4630150e2cb6c911c9aafb015a3986941
Zhiyuan Gao <837937787@qq.com>
2017-03-09 14:14:58 +0800

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script,first,second,third=argv
+print 'the script is called :',script
+print 'the first varible is:',first
+print 'the second varible is :',second
+print 'the third varible is:',third
---
+++ ex15.py
@@ -0,0 +1,11 @@
+from sys import argv
+script,filename=argv
+txt=open(filename)
+print id(txt)
+print "Here's your file %r:"%filename
+print txt.read()
+print "Type the filename again:"
+file_again=raw_input('> ')
+txt_again=open(file_again)
+print txt_again.read()
+print id(txt_again)
