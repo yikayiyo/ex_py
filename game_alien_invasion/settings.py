e66f94922c87db249b4a7fd2ae5e0881f1e714d1
Zhiyuan Gao <837937787@qq.com>
2017-05-16 21:55:16 +0800

    创建游戏窗口,响应用户输入,创建设置类
    todo:添加一些图片,重构gamefunction

---
+++ game_alien_invasion/settings.py
@@ -0,0 +1,10 @@
+#-*- ecoding:utf-8 -*-
+'''所有游戏设置'''
+class Settings():
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕的设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color=(230,230,230)
+
