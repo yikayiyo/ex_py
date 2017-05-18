ae94ab3d0cd938664e2d63a1f4fbb2e3e2a1fd76
Gao Zhiyuan <837937787@qq.com>
2017-05-18 17:32:13 +0800

    画上了飞船-0-

--- game_alien_invasion/alien_invasion.py
+++ game_alien_invasion/alien_invasion.py
@@ -3,12 +3,18 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings=Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(screen)
+
 
     #开始游戏的主循环
     while True:
@@ -19,7 +25,7 @@ def run_game():
 
         #重绘屏幕
         screen.fill(ai_settings.bg_color)
-
+        ship.blitme()
         #让最近绘制的屏幕可见
         pygame.display.flip()
 
