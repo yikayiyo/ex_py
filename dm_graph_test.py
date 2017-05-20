2188e022a75adf034653ccd11e8ecabb1cda42cd
yikayiyo <837937787@qq.com>
2017-05-20 21:31:19 +0800

    py algorithm

---
+++ dm_graph_test.py
@@ -0,0 +1,34 @@
+#-*- ecoding:utf-8 -*-
+'''图,广度遍历'''
+from collections import deque
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+#图
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+#借助队列实现BFS
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []#这个数组用于记录检查过的人
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:#避免重复检查
+            if person_is_seller(person):#随意设计的函数
+                print person + " is a mango seller!"
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+if __name__ == '__main__':
+    search("you")
