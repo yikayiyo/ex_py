4a702b2ec192b8c9035ffdb72751603680f63511
yikayiyo <837937787@qq.com>
2017-07-15 22:30:29 +0800

    set proxy

--- demo_scrapy/demo_scrapy/middlewares.py
+++ demo_scrapy/demo_scrapy/middlewares.py
@@ -54,3 +54,9 @@ class DemoSpiderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+class ProxyMiddleware(object):
+    # overwrite process request
+    def process_request(self, request, spider):
+        # Set the location of the proxy
+        request.meta['proxy'] = "http://127.0.0.1:8087"
--- demo_scrapy/demo_scrapy/useragent.py
+++ demo_scrapy/demo_scrapy/useragent.py
@@ -1,3 +1,4 @@
+import random
 def rand_agent():
     user_agent = [
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
