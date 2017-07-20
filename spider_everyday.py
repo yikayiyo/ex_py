dd81b6f9d4d14f06100971a1e8f8a7059123c6b2
yikayiyo <837937787@qq.com>
2017-07-20 21:07:44 +0800

    信息弹窗; 有bug:未点击就打开了页面

--- spider_everyday.py
+++ spider_everyday.py
@@ -1,41 +1,64 @@
-#-*- encoding:utf-8 -*-
+# -*- encoding:utf-8 -*-
 '''每天都可以爬的'''
-import requests,re,datetime
+from Tkinter import *
+import requests, re, datetime
+
+
 def free_ebook():
     '''图灵特价电子书'''
-    url='http://www.ituring.com.cn/'
+    url = 'http://www.ituring.com.cn/'
     try:
-        res=requests.get(url)
-        if 200==res.status_code:
-            html=res.content
+        res = requests.get(url)
+        if 200 == res.status_code:
+            html = res.content
     except:
-        html=None
+        html = None
     # print html
     if html:
-        pattern=re.compile(r'<dt>电子书每周特价</dt>.*?href="(.*?)">(.*?)</a>.*?href="(.*?)">(.*?)</a>.*?href="(.*?)">(.*?)</a>',re.S)
-        freebooks=re.search(pattern,html).groups()
-        bk1_name=freebooks[1]
-        bk1_link=freebooks[0]
-        bk2_name=freebooks[3]
-        bk2_link = freebooks[2]
-        bk3_name=freebooks[5]
-        bk3_link = freebooks[4]
-        mes=[(bk1_link,bk1_name),(bk2_link,bk2_name),(bk3_link,bk3_name)]
-        for item in mes:
-            print item
+        pattern = re.compile(
+            r'<dt>电子书每周特价</dt>.*?href="(.*?)">(.*?)</a>.*?href="(.*?)">(.*?)</a>.*?href="(.*?)">(.*?)</a>', re.S)
+        freebooks = re.search(pattern, html).groups()
+        bk1_link, bk1_name = freebooks[0], freebooks[1]
+        bk2_link, bk2_name = freebooks[2], freebooks[3]
+        bk3_link, bk3_name = freebooks[4], freebooks[5]
+        mes = [[bk1_link,bk1_name], [bk2_link,bk2_name], [bk3_link,bk3_name]]
         return mes
     else:
         print 'NO Info'
 
+
 def write_file(mes):
-    today=datetime.date.today()
-    filename='mes'+str(today)
-    print filename
-    with open(filename,'wb') as f:
-        f.write('图灵特价电子书\t'+str(today)+'\n')
-        for item in mes:
-            f.write(str(item[0])+'\t'+str(item[1])+'\n')
+    title = '图灵特价电子书'
+    today = datetime.date.today()
+    filename = 'mes' + str(today)
+    # print filename
+    with open(filename, 'wb') as f:
+        f.write(title + '\t' + str(today) + '\n')
+        for k in mes:
+            f.write(k[0] + '\t' + k[1] + '\n')
+
+
+def pop_mes(mes):
+    date = datetime.date.today()
+    window = Tk()
+    title = Label(window, text=str(date) + '特价电子书')
+    title.pack()
+    for k in mes:
+        # lambda event, text=text: self.click_link(event, text)
+        print k
+        book = Label(window,text=k[1]+k[0])
+        book.bind('Enter',openlink(k[0]))
+        book.pack()
+    window.mainloop()
+
+
+def openlink(url):
+    import webbrowser
+    webbrowser.open(url)
+    print '点击了' + url
+
 
 if __name__ == '__main__':
-    book_mes=free_ebook()
-    write_file(book_mes)
+    mes = free_ebook()
+    write_file(mes)
+    pop_mes(mes)
