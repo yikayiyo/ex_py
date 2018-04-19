ae83b9cfd829abeeaa724f020222b6dd49346cf5
Gao <837937787@qq.com>
2018-04-19 12:07:21 +0800

    Update dm_generator.py
    about 'yield'

--- dm_generator.py
+++ dm_generator.py
@@ -1,5 +1,10 @@
 #-*- ecoding:utf-8 -*-
-'''生成器'''
+'''生成器
+Yield is lazy, it puts off computation. A function with a yield in it doesn't actually execute at all when you call it. 
+The iterator object it returns uses magic to maintain the function's internal context.Each time you call next() on the 
+iterator (this happens in a for-loop) execution inches forward to the next yield. (return raises StopIteration and ends 
+the series.)
+'''
 class Bank():
     crisis = False
     def create_atm(self):
